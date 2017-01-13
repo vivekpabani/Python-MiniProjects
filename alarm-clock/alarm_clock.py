4f223c9f313430ec4154fd0296d4d33634de0dc3
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-13 11:32:51 -0600

    template setup

---
+++ alarm-clock/alarm_clock.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Description:
+
+Create an alarm clock, which takes the time to wake up, and performs an action when that time hits. 
+The action can be playing a video, or any notification action.
+"""
+__author__ = "vivek"
+
+
+import sys
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
+
+
+
